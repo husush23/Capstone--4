0a6f775bb0c9bcc99988080819eb7be003e8e1ad
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-08-16 16:53:44 +0100

    set up files for music album storage

---
+++ modules/display_music.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Module for displaying music functionality.
+module DisplayMusic
+  def display_all_music_albums
+    @music_albums.each_with_index do |music, index|
+      genre_name = music.genre ? music.genre.name : 'N/A'
+      puts "#{index}) on_Spotify: #{music.on_spotify}, Publish Date: #{music.publish_date} Genre: #{genre_name}"
+    end
+  end
+end
