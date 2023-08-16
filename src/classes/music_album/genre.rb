216838cbb040293d4f0ebb275551dc4f7867defe
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-08-16 14:40:49 +0100

    linter correction and other corrections

--- src/classes/music_album/genre.rb
+++ src/classes/music_album/genre.rb
@@ -1,18 +1,25 @@
 # frozen_string_literal: true
 
-require_relative './item'
-
 # Represents a genre of music albums.
 class Genre
-  attr_accessor :name, :items
+  attr_reader :name, :id
+  attr_accessor :items, :genre
 
-  def initialize(name)
+  def initialize(name:, id: nil)
+    @id = id || rand(1..1000)
     @name = name
     @items = []
   end
 
   def add_item(item)
-    item.genre = self
     @items << item
+    item.add_genre(self)
+  end
+
+  def to_hash
+    {
+      'id' => @id,
+      'name' => @name
+    }
   end
 end
--- src/classes/music_album/music_album.rb
+++ src/classes/music_album/music_album.rb
@@ -1,16 +1,27 @@
 # frozen_string_literal: true
-require_relative './item'
+
+require_relative '../item'
 
 # Represents a music album item.
 class MusicAlbum < Item
-  attr_accessor :on_spotify
+  attr_reader :on_spotify, :publish_date
+  attr_accessor :genre
 
-  def initialize(title, artist, release_year, on_spotify)
-    super(title, artist, release_year)
+  def initialize(on_spotify:, publish_date:, id: nil)
+    super(publish_date, id)
     @on_spotify = on_spotify
   end
 
   def can_be_archived?
-    super && @on_spotify
+    super() && @on_spotify
+  end
+
+  def to_hash
+    {
+      'id' => @id,
+      'publish_date' => @publish_date,
+      'on_spotify' => @on_spotify,
+      'genre' => @genre.name || ''
+    }
   end
 end
