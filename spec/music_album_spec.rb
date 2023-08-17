4f4f4762d5f639d6fd3f0e6199c8cebaf44e3d9b
husush23 <husseinkadare40@gmail.com>
2023-08-17 09:28:06 +0300

    Added more tests to genre

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -15,4 +15,31 @@ describe MusicAlbum do
           expect(music_album.publish_date).to eq('2023-08-17')
         end
       end
+
+      describe '#can_be_archived?' do
+        it 'returns false if the music album cannot be archived due to on_spotify' do
+          music_album = MusicAlbum.new(on_spotify: false, publish_date: '2019-08-17')
+          expect(music_album.can_be_archived?).to be(false)
+        end
+    
+        it 'returns false if the music album cannot be archived due to publish_date' do
+          music_album = MusicAlbum.new(on_spotify: true, publish_date: '2022-11-30')
+          expect(music_album.can_be_archived?).to be(false)
+        end
+      end
+
+      describe '#to_hash' do
+        it 'returns a hash representation of the music album' do
+          genre = Genre.new(name: 'Pop', id: 123)
+          music_album = MusicAlbum.new(on_spotify: true, publish_date: '2023-08-17')
+          music_album.genre = genre
+          expected_hash = {
+            'id' => music_album.id,
+            'publish_date' => '2023-08-17',
+            'on_spotify' => true,
+            'genre' => 'Pop'
+          }
+          expect(music_album.to_hash).to eq(expected_hash)
+        end
+      end
 end
